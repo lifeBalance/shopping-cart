cfe023c31c38ae50ac7b1284eea425dc1b41f663
javi <3051496+lifeBalance@users.noreply.github.com>
2023-04-05 00:01:46 +0300

    fix vite.config again

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shopping-cart",
-  publicDir: 'shopping-cart',
+  base: "/shopping-cart/", // as per the docs
   plugins: [react()],
 })
