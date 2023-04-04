c1fc83245b0988849588a700feba2a9eb0efaf5b
javi <3051496+lifeBalance@users.noreply.github.com>
2023-04-04 22:26:46 +0300

    add description in home and about pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,8 @@
 export function About() {
-  return <h1>about</h1>
+  return (
+    <>
+      <h1>about</h1>
+      <h2>Check the Store page son!</h2>
+    </>
+  )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,8 @@
 export function Home() {
-  return <h1>home</h1>
+  return (
+    <>
+      <h1>home</h1>
+      <h2>Check the Store page son!</h2>
+    </>
+  )
 }
