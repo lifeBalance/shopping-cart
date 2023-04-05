3a5f230abb3b3c891d724b2ecbe5d170489b767b
javi <3051496+lifeBalance@users.noreply.github.com>
2023-04-05 11:20:28 +0300

    set base to / in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shopping-cart/", // as per the docs
+  // base: "/shopping-cart/", // as per the docs
+  base: "/",
   plugins: [react()],
 })
