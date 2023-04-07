d21a069c2be0adbbfb3f1dcb0c6d1d9cdc258128
javi <3051496+lifeBalance@users.noreply.github.com>
2023-04-07 12:28:26 +0300

    one-more-try-for-real

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shopping-cart/", // as per the docs
+  base: "/", // as per the docs
   plugins: [react()],
 })
